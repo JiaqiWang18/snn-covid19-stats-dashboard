303726a634c1f849da4b8087f92a0773341a33c5
Jacky Wang <jacky200218@gmail.com>
2021-04-04 16:56:22 -0700

    use redux to handle changing metric type and date range

---
+++ src/components/TypeSelector.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+
+import { changeGraphMetricType } from "../actions";
+
+const options = [
+  {
+    label: "Total Cases",
+    value: "total_cases",
+  },
+  {
+    label: "Deaths",
+    value: "death",
+  },
+  {
+    label: "Recovered",
+    value: "recovered",
+  },
+];
+
+const TypeSelector = (props) => {
+  const [selected, setSelected] = useState(options[0]);
+  useEffect(() => {
+    console.log(selected.value);
+    props.changeGraphMetricType(selected.value);
+  }, [selected]);
+
+  const renderedOptions = options.map((option) => {
+    //hide item that is already selected
+    if (option.value === selected.value) return null;
+
+    return (
+      <a
+        className="dropdown-item"
+        key={option.value}
+        onClick={() => setSelected(option)}
+      >
+        {option.label}
+      </a>
+    );
+  });
+
+  return (
+    <div className="btn-group">
+      <button
+        type="button"
+        className="btn"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        {selected.label}
+      </button>
+      <div className="dropdown-menu">{renderedOptions}</div>
+    </div>
+  );
+};
+
+export default connect(null, {
+  changeGraphMetricType,
+})(TypeSelector);
